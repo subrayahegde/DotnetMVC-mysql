1794f1a55dd8594b077cc7bfe00199bd89b6a411
subrayahegde <hegdesaws@gmail.com>
2024-01-05 16:02:57 +0530

    first commit

---
+++ EmployeeCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,201 @@
+﻿using EmployeeCRUD.Data;
+using EmployeeCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+         public IActionResult Welcome()
+        {
+            return View();
+        }
+/*
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> objCatlist = _context.Employees;
+            return View(objCatlist);
+        }
+*/
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Employee> objCatlist = await _context.Employees.ToListAsync();
+            return View(objCatlist);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+/*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                var cdate=DateTime.Now;
+                //empobj.RecordCreatedOn = cdate;
+
+                _context.Employees.Add(empobj);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Record Added Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+*/
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                var cdate=DateTime.Now;
+                //empobj.RecordCreatedOn = cdate;
+
+                _context.Employees.Add(empobj);
+                await _context.SaveChangesAsync();
+                TempData["ResultOk"] = "Record Added Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+/*
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = _context.Employees.Find(id);
+
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+*/
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = await _context.Employees.FindAsync(id);
+
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+
+/*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(empobj);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Data Updated Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(empobj);
+                await _context.SaveChangesAsync();
+                TempData["ResultOk"] = "Data Updated Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+
+/*
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = _context.Employees.Find(id);
+         
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+*/
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = await _context.Employees.FindAsync(id);
+         
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+
+/*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteEmp(int? id)
+        {
+            var deleterecord = _context.Employees.Find(id);
+            if (deleterecord == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(deleterecord);
+            _context.SaveChanges();
+            TempData["ResultOk"] = "Data Deleted Successfully !";
+            return RedirectToAction("Index");
+        }
+*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteEmp(int? id)
+        {
+            var deleterecord = await _context.Employees.FindAsync(id);
+            if (deleterecord == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(deleterecord);
+            await _context.SaveChangesAsync();
+            TempData["ResultOk"] = "Data Deleted Successfully !";
+            return RedirectToAction("Index");
+        }
+        
+
+    }
+}
